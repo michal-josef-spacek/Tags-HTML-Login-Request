067a843762046f19e4970d064f4ad27ec1c8fa27
Michal Josef Špaček <michal.josef.spacek@gmail.com>
2024-05-18 09:37:23 +0200

    Prepare for 0.04 version

--- Request.pm
+++ Request.pm
@@ -14,7 +14,7 @@ use Tags::HTML::Messages;
 
 Readonly::Array our @FORM_METHODS => qw(post get);
 
-our $VERSION = 0.03;
+our $VERSION = 0.04;
 
 # Constructor.
 sub new {
@@ -576,6 +576,6 @@ BSD 2-Clause License
 
 =head1 VERSION
 
-0.03
+0.04
 
 =cut
